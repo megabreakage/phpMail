0d901c4eebec2ddd04958e3a49ee2049dbfbeb49
megabreakage <martin.njau@live.com>
2018-06-01 00:29:29 +0300

    creates sace_mail method to save emails before sending

--- application/models/Emails_model.php
+++ application/models/Emails_model.php
@@ -5,5 +5,9 @@ class Emails_model extends CI_Model {
   function __construct() {
     parent::__construct();
   }
+
+  public function save_mail($data){
+    return $this->db->insert('emails', $data);
+  }
 }
  ?>
